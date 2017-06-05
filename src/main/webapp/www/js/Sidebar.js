ccb5590425d3f3b4de928bc3808fb759036558dd
Sharayu Gandhi <gandhi16.sharayu@gmail.com>
2017-06-05 16:30:29 -0700

    Updates for Constant, Tried Paramter Label to move to left

--- src/main/webapp/www/js/Sidebar.js
+++ src/main/webapp/www/js/Sidebar.js
@@ -916,7 +916,7 @@ Sidebar.prototype.addGeneralPalette = function(expand)
 	 	//this.createVertexTemplateEntry('rhombus;whiteSpace=wrap;html=1;', 80, 80, '', 'Diamond', null, null, 'diamond rhombus if condition decision conditional question test'),
 	 	this.createEdgeTemplateEntry('endArrow=classic;html=1;', 50, 50, '', 'Connection'),
 	 	this.createVertexTemplateEntry('process;whiteSpace=wrap;html=1;', 120, 80, '', 'Process', null, null, 'oval ellipse state'),
-	 	this.createVertexTemplateEntry('parameter;whiteSpace=wrap;html=1;', 70, 60, '', 'Parameter', null, null, 'oval ellipse state'),
+	 	this.createVertexTemplateEntry('parameter;whiteSpace=nowrap;', 140, 60, '', 'Parameter', null, null, 'oval ellipse state'),
 	 	this.createVertexTemplateEntry('measurement;whiteSpace=wrap;html=1;', 70, 60, '', 'Measurement', null, null, 'oval ellipse state'),
 	 	this.createVertexTemplateEntry('fork;whiteSpace=wrap;html=1;', 120, 80, '', 'Fork', null, null, 'oval ellipse state'),
 	 	this.createVertexTemplateEntry('entity;whiteSpace=wrap;html=1;', 120, 80, '', 'Entity', null, null, 'oval ellipse state'),
